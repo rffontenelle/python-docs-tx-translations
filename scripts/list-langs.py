8d65949fe821ec4fb4cc924936ff76843dbfa810
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2023-07-27 19:44:59 -0300

    Propagate translations (#36)
    * Add translation propagation in ci.yml
    
    Adds more outputs to trigger job and adds the propagate_translations job
    
    * Fix extension of manage_versions.py in ci.yml
    
    * Fix step id of trigger job outputs in ci.yml
    
    * Try setting bash as shell
    
    * Fix .git dirname of languages python inliner in ci.yml
    
    * Print the outputs for debugging propose in ci.yml
    
    * Improve language printing
    
    * Add gitpython to requirements
    
    * Fix args use in list-langs.py
    
    * checkout the proper ref in ci.yml
    
    * Fiz checkout ref and matrix use in ci.yml
    
    * make sure requirements.txt is available for propagation in ci.yml
    
    * Remove safe-guard of tx push ci.yml
    
    * make pomerge a online command in ci.yml
    
    Also reduce list-langs.py line length using working-directory arg
    
    * Fix script running path in ci.yml
    
    * Handle missing lang dir in some branches in ci.yml
    
    * Try enable globstar shopt in ci.yml
    
    * Improve steps names in ci.yml

---
+++ scripts/list-langs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Prints the list of languages tracked in the repository."""
+
+from argparse import ArgumentParser
+import git
+import os
+
+def list_tracked_dirs(git_root):
+    """Get the list of tracked directories in the repository in *git_root*"""
+    repo = git.Repo(git_root)
+    tracked_files = repo.git.ls_tree("--full-tree", "--name-only", "HEAD").splitlines()
+    return tracked_files
+
+def filter_ignored_entries(file_list: list, ignored_entries: list) -> list:
+    """Filter out the ignored entries, to return the valid languages codes"""
+    filtered_files = [file_name for file_name in file_list if file_name not in ignored_entries]
+    return filtered_files
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--git-root', default=os.getcwd(), required=False)
+    args = parser.parse_args()
+
+    git_root = git.Repo(args.git_root, search_parent_directories=True).working_dir
+    ignored_entries = ['.tx', 'pot']
+
+    tracked_files = list_tracked_dirs(git_root)
+    languages = filter_ignored_entries(tracked_files, ignored_entries)
+    
+    print(languages)
