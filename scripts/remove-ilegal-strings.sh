f7360e27b3a091c528db6222bb831786fa775ae5
Rafael Fontenelle <rafaelff@gnome.org>
2024-05-11 17:36:54 -0300

    Simplify remove-ilegal-strings.sh

--- scripts/remove-ilegal-strings.sh
+++ scripts/remove-ilegal-strings.sh
@@ -2,17 +2,44 @@
 # Patch POT files to solve incompatibilities with Transifex
 # - \\N is treated as new line in Transifex, so it is an ilegal source string
 
-[ $# -lt 1 ] && { echo 'Expected one or more POT file as argument'; exit 1; }
-
-set -xeuo pipefail
+set -euo pipefail
 IFS=$'\n\t'
 
+# Check if a valid POT directory was passed in command-line interface.
+# If no argument is passed, use current working directory .
+if [ $# -lt 1 ]; then
+    if [[ "$(basename $PWD)" != "pot" ]]; then
+        echo "Error: Not in 'pot' directory and no path to pot directory passed."
+        exit 1
+    fi
+else
+    if [ $# -gt 1 ]; then
+        echo "Ignoring extra arguments, sticking with '$1' ..."
+    fi
+    if [[ ! -d $1 || "$(basename $1)" != "pot" ]]; then
+        echo "'$1' is not a valid directory. Expected a directory named 'pot'."
+    fi
+    cd $1
+fi
+
+# Create temporary files and set up exit trap
 tmp=$(mktemp --suffix='.po')
 tmp2=$(mktemp --suffix='.po')
-trap "rm -f $tmp $tmp2" EXIT
+trap 'rm -f $tmp $tmp2' EXIT
 
-for pot in $@; do
+
+# Remove '\\N', an ilegal string in Transifex, otherwise uplading POT fails
+remove_slash_n() {
+    pot=$1
     msggrep -Ke '^\\N$' $pot > $tmp
     msgcomm --no-wrap --less-than=2 $pot $tmp > $tmp2
     mv $tmp2 $pot
-done
+    echo "Removing '\\N' from: $pot"
+    powrap $pot
+}
+
+
+# Run patches in POT files known to need them
+remove_slash_n library/codecs.pot
+remove_slash_n library/re.pot
+remove_slash_n reference/lexical_analysis.pot
