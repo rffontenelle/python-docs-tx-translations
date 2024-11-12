f882a297164a590a7e0fb7db09e2124878098799
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2024-11-12 05:34:01 -0300

    Improve problematic source string removal (#98)
    * Rename remove-ilegal-strings.sh to remove-msgid.sh
    
    * remove-msgid: make flexible, remove more msgids

---
+++ scripts/remove-msgid.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Patch POT to solve issues with source messages
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Check if a valid POT directory was passed in command-line interface.
+# If no argument is passed, use current working directory .
+if [ $# -lt 1 ]; then
+    if [[ "$(basename $PWD)" != "pot" ]]; then
+        echo "Error: Not in 'pot' directory and no path to pot directory passed."
+        exit 1
+    fi
+else
+    if [ $# -gt 1 ]; then
+        echo "Ignoring extra arguments, sticking with '$1' ..."
+    fi
+    if [[ ! -d $1 || "$(basename $1)" != "pot" ]]; then
+        echo "'$1' is not a valid directory. Expected a directory named 'pot'."
+    fi
+
+    # Enter the given directory
+    cd $1
+fi
+
+# Create temporary files and set up exit trap
+tmp=$(mktemp --suffix='.po')
+tmp2=$(mktemp --suffix='.po')
+trap 'rm -f $tmp $tmp2' EXIT
+
+
+# Usage: remove_msgid </path/to/pot> <source message text>
+remove_msgid() {
+    pot=$1
+    shift
+    msgid=$@
+    msggrep -Ke "$msgid" $pot > $tmp
+    msgcomm --no-wrap --less-than=2 $pot $tmp > $tmp2
+    mv $tmp2 $pot
+    echo "Stripping from '$pot': $msgid"
+    powrap -q $pot
+}
+
+
+# \\N is treated as new line in Transifex, so it is an illegal source string
+# https://github.com/rffontenelle/python-docs-tx-translations/issues/15
+remove_msgid  library/codecs.pot '^\\N$'
+remove_msgid  library/re.pot  '^\\N$'
+remove_msgid  reference/lexical_analysis.pot  '^\\N$'
+
+# This string should not be translated, otherwise sphinx-build give warnings
+# Fixed via https://github.com/python/cpython/pull/19470
+remove_msgid  c-api/sys.pot '^Raises an :ref:`auditing event <auditing>` ``sys.addaudithook`` with no arguments.$'
+remove_msgid  library/sys.pot '^Raises an :ref:`auditing event <auditing>` ``sys.addaudithook`` with no arguments.$'
